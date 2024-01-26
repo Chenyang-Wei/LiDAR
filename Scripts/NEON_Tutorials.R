00c3c3efee53156bb301d1e0c322d035cebfc84f
Chenyang Wei <ChenyangWei.CWei@gmail.com>
2024-01-26 15:03:37 -0500

    Create an R script for the NEON LiDAR tutorials.

---
+++ Scripts/NEON_Tutorials.R
@@ -0,0 +1,12 @@
+# This script is based on the NEON tutorial series titled 
+#   "Introduction to Light Detection and Ranging (LiDAR) – 
+#   Explore Point Clouds and Work with LiDAR Raster Data in R"
+#   (https://www.neonscience.org/resources/learning-hub/tutorials/
+#   introduction-light-detection-and-ranging-lidar-explore-point).
+# 
+# Last updated: 1/26/2024.
+
+
+# Create a lidar-derived Canopy Height Model (CHM) ------------------------
+
+
